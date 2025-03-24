4b7b4368efe507417445fae55bd4e50fc38aabd8
Karina Turchyna <kari.tu99@gmail.com>
2025-03-24 17:26:26 +0200

    make build

---
+++ index.f0a4ecd8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.top="".concat((t.offsetHeight-e.offsetHeight)/2,"px"),e.style.left="".concat((t.offsetWidth-e.offsetWidth)/2,"px");
+//# sourceMappingURL=index.f0a4ecd8.js.map
