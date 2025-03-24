f25b6f9bf1b47fddeac909221873bfdc56765965
Karina Turchyna <kari.tu99@gmail.com>
2025-03-24 17:08:29 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+spider.style.top = wall.style.height / 2;
+spider.style.right = wall.style.width / 2;
