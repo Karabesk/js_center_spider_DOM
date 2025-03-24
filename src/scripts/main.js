61ed473f1d7c154d8659ee8d785e43549bc60839
Karina Turchyna <kari.tu99@gmail.com>
2025-03-24 17:26:12 +0200

    add replacements 4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,5 +4,5 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-spider.style.top = `${wall.offsetHeight - spider.offsetHeight / 2}px`;
-spider.style.left = `${wall.offsetWidth - spider.offsetWidth / 2}px`;
+spider.style.top = `${(wall.offsetHeight - spider.offsetHeight) / 2}px`;
+spider.style.left = `${(wall.offsetWidth - spider.offsetWidth) / 2}px`;
